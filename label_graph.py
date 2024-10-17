c5a98ac3b5d7d9282249d83c9a825af593e92ec2
Burke Johnson <gitlab.bur@cpu.8048.org>
2024-10-17 15:39:29 -0500

    remove functions that have been refactored into Graph methods in graph.py

--- label_graph.py
+++ label_graph.py
@@ -1,76 +1,87 @@
-from graph import MatrixGraph, Graph
+from graph import MatrixGraph, Graph, print_2d_matrix_graph, Node
+from copy import deepcopy
+import tree
+from prime_tools import prime_factors, rel_prime
 
 
-def prime_factors(x: int) -> list[int]:
-    l = [x]
+class GraphTreeNode(tree.Node):
+    """
+        extends 'tree.Node' to pack more data into 'tree.Node.value'
 
-    def divides(a, b):
-        return b // a == b / a
+        Stores a Graph object and a list of the integers that have not been used to label the graph yet.
 
-    last_cycle = False
-    while not last_cycle:
+        new methods:
+        get_graph() -> Graph
+        get_unassigned() -> list[int]
+    """
+    def __init__(self, g: Graph = None, unassigned: list[int] = None, parent: "Node" = None):
+        super().__init__({"graph": g, "unassigned": unassigned}, parent)
 
-        last_cycle = True
+    def get_graph(self):
+        return self.value["graph"]
 
-        for i in range(len(l)):
-            for j in range(2, l[i]):
-                if divides(j, l[i]):
-                    last_cycle = False
-                    l.append(j)
-                    l[i] //= j
+    def get_unassigned(self):
+        return self.value["unassigned"]
 
-    l_filtered = list(
-        x for x in l if x != 1  # I don't know why, but 1's randomly end up in here
-    )
+def free_adj_space(g: Graph) -> list[int] | None:
+    """
+        Gets a list of indexes of free nodes in a graph that are adjacent to non-free nodes
 
-    l_filtered.append(1)
+        If there are no free nodes adjacent to non-free nodes (if the graph is empty/full)
+        returns None
+    """
 
-    return sorted(l_filtered)
+    unoccupied_i = (i for i in range(len(g.nodes)) if g.nodes[i].value is None or g.nodes[i].value == 0)
+    occupied_adj_i = []
+
+    for i in unoccupied_i:
+        neighbor_values = (neighbor.value for neighbor in g.nodes[i].neighbors if neighbor.value is not None)
+
+        for value in neighbor_values:
+            if value > 0:
+                occupied_adj_i.append(i)
+
+    if not len(occupied_adj_i):
+        return None
+    return occupied_adj_i
+
+# def permutate_tree_node(n: tree.Node, unassigned: list[int]):
+#     if graph_is_empty(n.value):
+#         for i in range(len(n.value.nodes)):
 
 
-def rel_prime(x: int, y: int):
-    x_fact = set(prime_factors(x))
-    y_fact = set(prime_factors(y))
+def get_prime_labelling(*dims: int) -> MatrixGraph | None:
+    """
+    Takes an arbitrary number of dimensions, and generates a MatrixGraph accordingly
 
-    for i in x_fact:
-        if i in y_fact and i != 1:
-            return False
+    Attempts to find a valid prime labelling of the graph, and returns the resultant MatrixGraph object
+    or None if no labelling was found.
+    :param dims:
+    :return:
+    """
+    root_graph = MatrixGraph(*dims)
 
-    return True
+    nums = [i + 1 for i in range(len(root_graph.nodes))]
+    nodes_index_rank = sorted(range(len(root_graph.nodes)),
+                              key=lambda x: len(root_graph.nodes[x].neighbors))  # less -> more neighbors
+    nums_rank = sorted(nums, key=lambda x: len(set(prime_factors(x))))  # less -> more unique prime factors
 
-def generate_graph_permutations(g: Graph):
-    occupied = (node for node in g.nodes if node.value is not None)
-    occupied_adj = []
+    tree_root = tree.Node(root_graph)
 
-    for node in occupied:
-        for neighbor in node.neighbors:
-            if neighbor.value is None or neighbor.value == 0:
-                occupied_adj.append(neighbor)
 
-    free_nodes = set(occupied_adj)
+def main():
+    print("input dimensions (m x n):")
 
-    ### I've generated a list of the free nodes adjacent to filled nodes, but this function is not finished
-    ### It must handle receiving an empty graph (select the node with most or least connections, I'd guess)
-    ### I must also figure out how to tell this function which number to place in the empty spot in each permutation
-    ### maybe it can just return a list of next node indexes?
+    m = int(input("m: "))
+    n = int(input("n: "))
 
-print("input dimensions (m x n):")
+    labelled_graph = get_prime_labelling(m, n)
 
-m = int(input("m: "))
-n = int(input("n: "))
+    if labelled_graph is None:
+        print("No prime labelling was found")
+    else:
+        print_2d_matrix_graph(labelled_graph)
 
-the_graph = MatrixGraph(m, n)
 
-nums = [i + 1 for i in range(m * n)]
-nums_factored = [prime_factors(n) for n in nums]
-
-nodes = the_graph.nodes
-nodes_rank = sorted(nodes, key=lambda x: len(x.neighbors))
-nodes_index_rank = sorted(range(len(the_graph.nodes)), key=lambda x: len(the_graph.nodes[x].neighbors))
-
-nums_rank = sorted(nums, key=lambda x: len(set(prime_factors(x))))
-
-for num in nums_rank:
-    print(num)
-
-graphs = []
+if __name__ == "__main__":
+    main()
