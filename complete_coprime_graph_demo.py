89d6d8b55b324b16bbb143898bde4d5e20cc2ff8
Burke Johnson <185158560+synth-mania@users.noreply.github.com>
2024-10-18 14:43:28 -0500

    Moved the CompleteCoprimeGraph class definition to graph.py

---
+++ complete_coprime_graph_demo.py
@@ -0,0 +1,5 @@
+from graph import CompleteCoprimeGraph
+
+print("complete coprime graph demonstration\n")
+n = int(input("How many nodes in the graph? : "))
+CompleteCoprimeGraph(n).print_linked_value()
--- graph.py
+++ graph.py
@@ -162,6 +162,39 @@ class Graph:
             print(f"{node.get_value()}: ", " ".join(str(x) for x in (i.get_value() for i in node.get_neighbors())))
 
 
+class CompleteCoprimeGraph(Graph):
+    """
+        Defines Graph with n nodes labelled 1 through n
+
+        Every node is connected to every other node for which their values are coprime
+    """
+    def __init__(self, n: int):
+
+        # create n nodes
+        nodes = Node.nodes(n)
+
+        # initialize Graph with nodes
+        super().__init__(*nodes)
+
+        # label each node
+        for i in range(self.size):
+            self.get_node(i).set_value(i+1)
+
+        # link every node to every other node it with which it is coprime
+        for i in range(self.size - 1):
+            for j in range(i + 1, self.size):
+                # print(f"Checking {i+1}, {j+1}")
+                if coprime(i+1, j+1):
+
+                    self.get_node(i).neighbors.append(
+                        self.get_node(j)
+                    )
+
+                    self.get_node(j).neighbors.append(
+                        self.get_node(i)
+                    )
+
+
 class MatrixGraph(Graph):
     """
         Defines a graph where the nodes, if each occupied a space on
